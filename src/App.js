4fcb8fbfe87f83efaaed03e2a48343815ce317cf
Maciej Wódke <maciej@mwpro.pl>
2018-05-05 15:57:14 +0200

    Remove Home component, cleanup App

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
 import { hot } from 'react-hot-loader';
 import * as React from 'react';
-import Home from './view/Home';
 
 import launch from './assets/launch.json';
 import launchSite from './assets/launch_site.json';
 import rocket from './assets/rocket.json';
 
 import './styles/theme.sass';
-import Counter from './components/Counter';
+
 import LaunchDetails from './view/LaunchDetails'
 
 class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  constructor(props) {
-    super();
-
-    this.onTimerFinish = this.onTimerFinish.bind(this);
-  }
-  
-  state = { timerFinished: false }
-
-  onTimerFinish() {
-    this.setState({ timerFinished: true });
-  }
-
   render() {
-    const potatoes = this.state.timerFinished ?
-      (<div className="cookingResult">🥔 ➡ 🍽️</div>) : "";
-
     return (
       <main>
-        <Home username="DaftCoder" />
-        <Counter from={64} to={54} onSuccess={this.onTimerFinish} />
-        {potatoes}
         <LaunchDetails
           launch={launch}
           launchSite={launchSite}
